2bb9afd20a86f01534e5ff9805b049307e8e6f36
ks-vikas <vikassaini140@gmail.com>
2023-09-27 13:18:10 +0530

    Enemy runners and spawner included.

--- Assets/Scripts/Enemy/EnemyRunLeft.cs
+++ Assets/Scripts/Enemy/EnemyRunLeft.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy1 : MonoBehaviour
+public class EnemyRunLeft : MonoBehaviour
 {
 
 
@@ -10,15 +10,11 @@ public class Enemy1 : MonoBehaviour
 
     private Rigidbody2D rb;
 
-    // Start is called before the first frame update
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-
-        speed = 7;
     }
 
-    // Update is called once per frame
     void Update()
     {
         rb.velocity = new Vector2(-speed, rb.velocity.y);
---
+++ Assets/Scripts/Enemy/SpawnerEnemy.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerEnemy : MonoBehaviour
+{
+    [SerializeField] private GameObject[] enemiesReference;
+    [SerializeField] private Transform leftPosition, rightPosition;
+    [SerializeField] private int numberOfEnemies = 5;
+
+    private GameObject spawnedEnemy;
+    private GameObject player;
+
+    private int randIndex;
+    private int randSide;
+    private float playerPosX;
+    private float spawnerPosX;
+    private bool isSpawned = false;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Update()
+    {
+        playerPosX = player.transform.position.x;
+        spawnerPosX = transform.position.x;
+
+        if ( !isSpawned && (spawnerPosX - playerPosX < 30f) && (playerPosX < spawnerPosX) )
+        {
+            isSpawned = true;
+            StartCoroutine(SpawnEnemy());
+        }
+    }
+    IEnumerator SpawnEnemy()
+    {
+        
+        while (numberOfEnemies-- > 0)
+        {
+            yield return new WaitForSeconds(Random.Range(1, 5));
+            randIndex = Random.Range(0, enemiesReference.Length);
+            randSide = Random.Range(0, 2);
+
+            spawnedEnemy = Instantiate(enemiesReference[randIndex]);
+
+            if (randSide == 0)
+            { //left side 
+                spawnedEnemy.transform.position = leftPosition.position;
+                spawnedEnemy.GetComponent<EnemyRunLeft>().speed = Random.Range(4, 10);
+                
+
+            }
+            else
+            {
+                //right side
+                spawnedEnemy.transform.position = rightPosition.position;
+                spawnedEnemy.GetComponent<EnemyRunLeft>().speed = -Random.Range(4, 10);
+                spawnedEnemy.transform.localScale = new Vector3(-5f, 5f, 5f);
+
+            }
+        }
+
+    }
+}
